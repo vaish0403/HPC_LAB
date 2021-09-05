040b22d29c98f7632ee5a8605567880958ad0e73
vaish0403 <57799996+vaish0403@users.noreply.github.com>
2021-09-05 23:26:12 +0530

    Create 2018BTECS00060_P2PS2.c

---
+++ PracticalNo2/2018BTECS00060_P2PS2.c
@@ -0,0 +1,48 @@
+//2018BTECs00060
+//Addition of  2 vectors Work Sharing
+#include <stdlib.h>  
+#include <stdio.h>    
+#include <omp.h>      
+
+
+#define ARRAY_SIZE 8     
+#define NUM_THREADS 4    
+
+int main (int argc, char *argv[]) 
+{
+	int * a;
+	int * b; 
+	int * c;
+        
+    int n = ARRAY_SIZE;                 
+	int n_per_thread;                   
+	int total_threads = NUM_THREADS;    
+	int i;       
+    a = (int *) malloc(sizeof(int)*n);
+	b = (int *) malloc(sizeof(int)*n);
+	c = (int *) malloc(sizeof(int)*n);
+        for(i=0; i<n; i++) {
+            a[i] = i;
+        }
+        for(i=0; i<n; i++) {
+            b[i] = i;
+        }   
+	omp_set_num_threads(total_threads);
+	
+	
+	n_per_thread = n/total_threads;
+	#pragma omp parallel for shared(a, b, c) private(i) schedule(static, n_per_thread)
+        for(i=0; i<n; i++) {
+		c[i] = a[i]+b[i];
+		printf("Thread %d works on element%d\n", omp_get_thread_num(), i);
+        }
+	printf("i\ta[i]\t+\tb[i]\t=\tc[i]\n");
+        for(i=0; i<n; i++) {
+		printf("%d\t%d\t\t%d\t\t%d\n", i, a[i], b[i], c[i]);
+        }
+	
+        // clean up memory
+        free(a);  free(b); free(c);
+	
+	return 0;
+}
