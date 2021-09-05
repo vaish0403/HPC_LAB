040693694d3b6114c9d86faa5d6daf6bf8ebcb90
vaish0403 <57799996+vaish0403@users.noreply.github.com>
2021-09-05 19:37:17 +0530

    Create 2018BTECS00060_P1.c

---
+++ PracticalNo1/2018BTECS00060_P1.c
@@ -0,0 +1,15 @@
+//2018BTECS00060
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+// Beginning of parallel region
+    #pragma omp parallel
+    {
+        printf("Hello World, PRN: 2018BTECS00060 from thread = %d\n", omp_get_thread_num());
+    }
+    return 0;
+// Ending of parallel region
+}
