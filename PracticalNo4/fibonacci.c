aacd451ab20a45f62680c67cd32c86c24534bd19
Vaishnavi Shahaji Patil <57799996+vaish0403@users.noreply.github.com>
2021-09-28 22:11:15 +0530

    Update fibonacci.c

--- PracticalNo4/fibonacci.c
+++ PracticalNo4/fibonacci.c
@@ -1,1 +1,36 @@
 
+//2018BTECS00060
+
+//Analyse and implement a Parallel code for below programs using OpenMP considering synchronization requirements. 
+//Fibonacci Computation (Using Dynamic Programming)
+
+
+#include<stdio.h>
+#include<omp.h>
+
+int fib(int n)
+{
+    /* Declare an array to store Fibonacci numbers. */
+    int f[n+2]; // 1 extra to handle case, n = 0
+    int i;
+    /* 0th and 1st number of the series are 0 and 1*/
+    f[0] = 0;
+    f[1] = 1;
+    #pragma omp ordered
+        for (i = 2; i <= n; i++)
+        {
+            f[i] = f[i-1] + f[i-2];
+        }
+     return f[n];
+}
+
+int main ()
+{
+    // int n = 9;
+	int n;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+    printf("%dth fibonacci number is : %d\n", n, fib(n));
+    // getchar();
+    return 0;
+}
