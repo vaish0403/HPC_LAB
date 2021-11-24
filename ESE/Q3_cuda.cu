c264f19c942f83f30a9bd994d5869c5a7a7909ba
Vaishnavi Shahaji Patil <57799996+vaish0403@users.noreply.github.com>
2021-11-24 15:49:05 +0530

    Update Q3_cuda.cu

--- ESE/Q3_cuda.cu
+++ ESE/Q3_cuda.cu
@@ -30,19 +30,15 @@ int main()
 					    printf("%d ", b[i]);
         }
 
-/* cudaMalloc() allocates memory from Global memory on GPU */
     cudaMalloc((void **)&d,6*sizeof(int));
     cudaMalloc((void **)&e,6*sizeof(int));
     cudaMalloc((void **)&f,6*sizeof(int));
 
-/* cudaMemcpy() copies the contents from destination to source. Here destination is GPU(d,e) and source is CPU(a,b) */
  cudaMemcpy(d,a,6*sizeof(int),cudaMemcpyHostToDevice);   
  cudaMemcpy(e,b,6*sizeof(int),cudaMemcpyHostToDevice);
  
-/* call to kernel. Here 6 is number of blocks, 1 is the number of threads per block and d,e,f are the arguments */ 
 arradd<<<6,1>>>(d,e,f); 
 
-/* Here we are copying content from GPU(Device) to CPU(Host) */
  cudaMemcpy(c,f,6*sizeof(int),cudaMemcpyDeviceToHost);
     
 printf("\nSum :\n ");
@@ -50,8 +46,6 @@ printf("\nSum :\n ");
     {
         printf("%d\t",c[i]);
     }
-
-/* Free the memory allocated to pointers d,e,f */
     cudaFree(d);
     cudaFree(e);
     cudaFree(f);
